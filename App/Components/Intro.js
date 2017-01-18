41ab4e9f86da6ab16f9c3cb9a50f79356c435378
Kane Rogers <kane.m.rogers@gmail.com>
2017-01-19 09:59:13 +1100

    Add Component

---
+++ App/Components/Intro.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export default class extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      screen: 0,
+    };
+  }
+
+  render() {
+    return (
+      <View />
+    );
+  }
+}
