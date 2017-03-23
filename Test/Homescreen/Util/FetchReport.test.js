e7a58a558da00ae581ead4f6165ddef974335b2a
Kane Rogers <kane.m.rogers@gmail.com>
2017-03-24 10:12:15 +1100

    Begin integrating lambda.

--- Test/Homescreen/Util/FetchReport.test.js
+++ Test/Homescreen/Util/FetchReport.test.js
@@ -3,12 +3,15 @@ import base64 from 'base-64';
 
 describe('#FetchReport', () => {
   it('fetches a report', done => {
+    const testContext = { UUID: 'test-uuid' };
     const callback = data => {
-      expect(data).toBeTruthy();
+      expect(data.payload).toEqual(testContext);
       done();
     };
 
-    const context = base64.encode(JSON.stringify({ UUID: 'test-uuid' }));
+    const context = base64.encode(JSON.stringify(testContext));
+
+    console.log(context);
 
     fetchReport(context, callback);
   });
