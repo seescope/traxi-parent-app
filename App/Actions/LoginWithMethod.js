b1cdb7953e5746b6b6ffb1366367336cea3034ab
Kane Rogers <kane.m.rogers@gmail.com>
2017-04-18 17:54:39 +1000

    Deleted unnecessary files.

--- App/Actions/LoginWithMethod.js
+++
@@ -1,7 +0,0 @@
-import { NativeModules } from 'react-native';
-
-export default () =>
-  dispatch => NativeModules.Authentication.authenticate().then(profile => {
-    dispatch({ type: 'LOGGED_IN', profile });
-    return profile;
-  });
