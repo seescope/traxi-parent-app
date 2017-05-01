707628b18b8b7e747c4d9685b729ccf7a50c2cd1
Kane Rogers <kane.m.rogers@gmail.com>
2017-05-01 12:52:37 +1000

    WIP MigrateDataFromPreviousVersion

--- App/AsyncActions/MigrateDataFromPreviousVersion.js
+++ App/AsyncActions/MigrateDataFromPreviousVersion.js
@@ -1,25 +1,83 @@
 // @flow
 import * as Firebase from 'firebase';
-import type { KidsState } from '../Reducers/Kids';
-import type { SetupState } from '../Reducers/Setup';
-
-// NOTE: Importing RootState from '../Reducers' does not seem to work?
-type RootState = {
-  kidsState: KidsState,
-  setupState: SetupState,
-};
+import lodash from 'lodash';
+import type { Kid, KidsState, DeviceType } from '../Reducers/Kids';
+import type { ParentState } from '../Reducers/Parent';
+import { profileMigrated } from '../Reducers/Parent/ParentActions';
 
 type Dispatch = () => void;
-type GetState = () => RootState;
 
-export default () =>
-  (dispatch: Dispatch, getState: GetState) => {
-    const { setupState, kidsState } = getState();
+type ProfileFromAsyncStorage = {
+  UUID: string,
+};
+type KidFromFirebase = {
+  name: string,
+  avatarURL: string,
+  UUID: string,
+  deviceType: DeviceType,
+  status: string,
+};
+type FirebaseKids = {
+  [string]: KidFromFirebase,
+};
+type ProfileFromFirebase = {
+  name: string,
+  kids: FirebaseKids,
+  UUID: string,
+  email: string,
+};
 
-    const { kidUUID } = setupState;
-    if (!kidUUID) return Promise.reject('No kid UUID!');
+type ConvertedProfile = {
+  parent: ParentState,
+  kids: KidsState,
+};
 
-    const kid = kidsState[kidUUID];
+const getKidUUIDs = (kids: FirebaseKids): Array<string> =>
+  lodash.chain(kids).mapValues('UUID').values().value();
 
-    return Firebase.database().ref(`kids/${kidUUID}`).set(kid);
+const convertParent = (profile: ProfileFromFirebase): ParentState => ({
+  UUID: profile.UUID,
+  name: profile.name,
+  email: profile.email,
+  kids: getKidUUIDs(profile.kids),
+  password: undefined,
+});
+
+const convertKid = (kid: KidFromFirebase): Kid => ({
+  UUID: kid.UUID,
+  name: kid.name,
+  installed: kid.status === 'INSTALLED',
+  avatarURL: kid.avatarURL,
+  deviceType: kid.deviceType,
+});
+
+const reduceKid = (result: KidsState, kid) => ({
+  ...result,
+  [kid.UUID]: convertKid(kid),
+});
+
+const convertKids = (kids: FirebaseKids): KidsState =>
+  lodash.reduce(kids, reduceKid, {});
+
+const convertProfile = (profile: ProfileFromFirebase): ConvertedProfile => {
+  const parent = convertParent(profile);
+  const kids = convertKids(profile.kids);
+
+  return {
+    parent,
+    kids,
+  };
+};
+
+export default (profile: ProfileFromAsyncStorage) =>
+  async (dispatch: Dispatch) => {
+    const { UUID } = profile;
+    const firebaseResult = await Firebase.database()
+      .ref(`parents/${UUID}`)
+      .once('val');
+
+    const profileFromFirebase: ProfileFromFirebase = firebaseResult.val();
+    const { parent, kids } = convertProfile(profileFromFirebase);
+
+    return dispatch(profileMigrated(parent, kids));
   };
--- App/AsyncActions/__tests__/MigrateDataFromPreviousVersion.test.js
+++ App/AsyncActions/__tests__/MigrateDataFromPreviousVersion.test.js
@@ -1,15 +1,33 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import migrateDataFromPreviousVersion from '../MigrateDataFromPreviousVersion';
-import { mockSet } from 'firebase';
 
-const TEST_KID = {
-  UUID: 'abc-123',
-  name: 'Jeff',
+const EXPECTED_PARENT = {
+  UUID: 'YwS0vJ8OE8N6yenxHaV6PdMVLbG3',
+  email: 'enakudesu@gmail.com',
+  kids: [
+    '2f566920-f598-46d2-8bf2-7bcae115bf0a',
+    '886b79ea-3572-4b4a-9c25-59b14639ac3d',
+  ],
+  name: 'Kane Rogers',
+  password: undefined,
 };
 
-const TEST_SETUP_STATE = {
-  kidUUID: TEST_KID.UUID,
+const EXPECTED_KIDS = {
+  '2f566920-f598-46d2-8bf2-7bcae115bf0a': {
+    UUID: '2f566920-f598-46d2-8bf2-7bcae115bf0a',
+    avatarURL: 'http://i.imgur.com/52mRwuE.jpg',
+    deviceType: 'Android',
+    installed: true,
+    name: 'Sam',
+  },
+  '886b79ea-3572-4b4a-9c25-59b14639ac3d': {
+    UUID: '886b79ea-3572-4b4a-9c25-59b14639ac3d',
+    avatarURL: 'https://tiestoclublife.files.wordpress.com/2008/08/dj-tiesto-club-life.jpeg',
+    deviceType: undefined,
+    installed: false,
+    name: 'DJ',
+  },
 };
 
 describe('MigrateDataFromPreviousVersion', () => {
@@ -25,16 +43,9 @@ describe('MigrateDataFromPreviousVersion', () => {
       .dispatch(migrateDataFromPreviousVersion(TEST_PROFILE))
       .then(() => {
         const action = store.getActions()[0];
-        expect(action.type).toEqual('MIGRATE_DATA_FROM_PREVIOUS_VERSION');
-        expect(action.parent).toEqual({
-          UUID: 'abc-123',
-          name: 'Test Parent',
-          kids: ['abc-123'],
-        });
-        expect(action.kid).toEqual({
-          UUID: 'abc-123',
-          name: 'Test Kid',
-        });
+        expect(action.type).toEqual('PROFILE_MIGRATED');
+        expect(action.parent).toEqual(EXPECTED_PARENT);
+        expect(action.kids).toEqual(EXPECTED_KIDS);
       });
   });
 });
