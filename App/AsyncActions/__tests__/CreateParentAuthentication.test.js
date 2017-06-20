0cf4d4c3d41654ecbd4077ddd2ff8a66fbfca58a
Christopher Histe <chrishiste@gmail.com>
2017-06-20 11:58:08 +1000

    Updated test

--- App/AsyncActions/__tests__/CreateParentAuthentication.test.js
+++ App/AsyncActions/__tests__/CreateParentAuthentication.test.js
@@ -8,36 +8,51 @@ import createParentAuthentication, {
 } from '../CreateParentAuthentication';
 import { mockCreateUser, mockUpdateProfile } from 'firebase';
 
-const TEST_PARENT = {
-  email: 'something@something.com',
-  password: 'something',
-  UUID: 'abc-123',
-  name: 'Jeff',
+const fetchFromStoreAndCreateFirebaseUser = TEST_PARENT => {
+  const mockStore = configureMockStore([thunk]);
+  const store = mockStore({
+    parentState: TEST_PARENT,
+  });
+
+  return store.dispatch(createParentAuthentication()).then(() => {
+    expect(mockCreateUser).toHaveBeenCalledWith(
+      TEST_PARENT.email,
+      TEST_PARENT.password,
+    );
+    expect(mockUpdateProfile).toHaveBeenCalledWith({
+      displayName: TEST_PARENT.name,
+    });
+
+    expect(store.getActions()[0].type).toEqual('TEST_USER_LOGGED_IN');
+  });
 };
 
 describe('CreateParentAuthentication', () => {
   test('Fetches the Parent from the store and creates a Firebase user', () => {
-    const mockStore = configureMockStore([thunk]);
-    const store = mockStore({
-      parentState: TEST_PARENT,
-    });
+    const TEST_PARENT = {
+      email: 'something@something.com',
+      password: 'something',
+      UUID: 'abc-123',
+      name: 'Jeff',
+    };
 
-    return store.dispatch(createParentAuthentication()).then(() => {
-      expect(mockCreateUser).toHaveBeenCalledWith(
-        TEST_PARENT.email,
-        TEST_PARENT.password,
-      );
-      expect(mockUpdateProfile).toHaveBeenCalledWith({
-        displayName: TEST_PARENT.name,
-      });
-
-      expect(store.getActions()[0].type).toEqual('TEST_USER_LOGGED_IN');
-    });
+    fetchFromStoreAndCreateFirebaseUser(TEST_PARENT);
   });
 
   test('Trims whitespace from email correctly', () => {
     const email = ' something@something.com ';
     const expected = 'something@something.com';
     expect(removeWhiteSpaceFromEmail(email)).toBe(expected);
+  });
+
+  test('Fetches the Parent from the store and creates a Firebase user correctly with spaces in the email', () => {
+    const TEST_PARENT = {
+      email: ' something@something.com ',
+      password: 'something',
+      UUID: 'abc-123',
+      name: 'Jeff',
+    };
+
+    fetchFromStoreAndCreateFirebaseUser(TEST_PARENT);
   });
 });
