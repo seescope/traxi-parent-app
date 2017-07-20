8d941e316ea8caffed0c7b9454cf9bf0480e0485
Kane Rogers <kane.m.rogers@gmail.com>
2017-07-20 13:29:31 +1000

    Fix SignUp test.

--- Test/Containers/SignUp.test.js
+++ Test/Containers/SignUp.test.js
@@ -1,8 +1,8 @@
 jest.mock('../../App/AsyncActions/PersistParent', () => jest.fn());
-jest.mock('../../App/AsyncActions/FetchReports', () => jest.fn());
+jest.mock('../../App/AsyncActions/PersistKid', () => jest.fn());
+jest.mock('../../App/AsyncActions/WatchKid', () => jest.fn());
 jest.mock('../../App/AsyncActions/CreateParentAuthentication', () => jest.fn());
 
-import OneSignal from 'react-native-onesignal';
 import { Alert } from 'react-native';
 import React from 'react';
 import renderer from 'react-test-renderer';
@@ -10,14 +10,13 @@ import { Actions } from 'react-native-router-flux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
+import mockWatchKid from '../../App/AsyncActions/WatchKid';
 import mockPersistParent from '../../App/AsyncActions/PersistParent';
-import mockFetchReports from '../../App/AsyncActions/FetchReports';
+import mockPersistKid from '../../App/AsyncActions/PersistKid';
 import mockCreateParentAuthentication
   from '../../App/AsyncActions/CreateParentAuthentication';
 
-import SignUpComponent, {
-  mapDispatchToProps,
-} from '../../App/Containers/SignUp';
+import SignUpComponent, { mergeProps } from '../../App/Containers/SignUp';
 
 Alert.alert = jest.fn();
 
@@ -47,18 +46,18 @@ describe('SignUp', () => {
   });
 });
 
-describe('mapDispatchToProps', () => {
-  it('onPress handles loading calls createParentAuthentication and persistParent, then goes to dashboard', () => {
+describe('mergeProps', () => {
+  it('onCompleteSetup handles loading calls createParentAuthentication and persistParent, then goes to dashboard', () => {
     const mockDispatch = jest.fn(() => Promise.resolve());
-    const { onPress } = mapDispatchToProps(mockDispatch);
-    return onPress().then(() => {
+    const { onCompleteSetup } = mergeProps({}, { dispatch: mockDispatch });
+    return onCompleteSetup().then(() => {
       expect(mockDispatch).toHaveBeenCalledWith({ type: 'STARTED_LOADING' });
       expect(mockDispatch).toHaveBeenCalledWith({ type: 'STOPPED_LOADING' });
       expect(mockCreateParentAuthentication).toHaveBeenCalled();
       expect(mockPersistParent).toHaveBeenCalled();
-      expect(mockFetchReports).toHaveBeenCalled();
-      expect(Actions.dashboard).toHaveBeenCalled();
-      expect(OneSignal.registerForPushNotifications).toHaveBeenCalled();
+      expect(mockPersistKid).toHaveBeenCalled();
+      expect(mockWatchKid).toHaveBeenCalled();
+      expect(Actions.checkForDevice).toHaveBeenCalled();
     });
   });
 
@@ -66,8 +65,8 @@ describe('mapDispatchToProps', () => {
     const testErrorMessage = 'Intentional test error - ignore!';
     const mockDispatch = jest.fn(() =>
       Promise.reject(new Error(testErrorMessage)));
-    const { onPress } = mapDispatchToProps(mockDispatch);
-    return onPress().then(() => {
+    const { onCompleteSetup } = mergeProps({}, { dispatch: mockDispatch });
+    return onCompleteSetup().then(() => {
       expect(mockDispatch).toHaveBeenCalledWith({ type: 'STARTED_LOADING' });
       expect(mockDispatch).toHaveBeenCalledWith({ type: 'STOPPED_LOADING' });
       expect(Alert.alert).toHaveBeenCalledWith(testErrorMessage);
