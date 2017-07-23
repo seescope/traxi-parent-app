badab4ac6a6ea447ca873ae1bfbc02729b16f2ab
Kane Rogers <kane.m.rogers@gmail.com>
2017-07-23 19:57:20 +1000

    Re-work SendReminder
    - No need to input email
    - Make sure we go back to the right screen

--- Test/Containers/SendReminder.test.js
+++ Test/Containers/SendReminder.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { Keyboard, Alert } from 'react-native';
+import { Alert } from 'react-native';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
@@ -8,10 +8,7 @@ jest.mock('../../App/AsyncActions/RequestReminder', () =>
   jest.fn(email => Promise.resolve({ type: 'REMINDER_REQUESTED', email })));
 import RequestReminder from '../../App/AsyncActions/RequestReminder';
 
-import SendReminder, {
-  mergeProps,
-  verifyEmail,
-} from '../../App/Containers/SendReminder';
+import SendReminder, { mergeProps } from '../../App/Containers/SendReminder';
 
 const mockStore = configureStore([thunk]);
 const testStore = mockStore({
@@ -39,41 +36,13 @@ it('renders the <SendReminder> component', () => {
   expect(tree).toMatchSnapshot();
 });
 
-it('handles setting the parent\'s email', () => {
-  const mockDispatch = jest.fn();
-
-  const { onEmailChanged } = mergeProps({}, { dispatch: mockDispatch });
-  onEmailChanged('test@email.com');
-  expect(mockDispatch).toHaveBeenCalledWith({
-    type: 'SET_EMAIL',
-    email: 'test@email.com',
-  });
-});
-
-it('verifies the email was input correctly', () => {
-  Keyboard.dismiss = jest.fn();
-  Alert.alert = jest.fn();
-
-  verifyEmail('test@email.com');
-  expect(Keyboard.dismiss).toHaveBeenCalled();
-  expect(Alert.alert).not.toHaveBeenCalled();
-
-  Keyboard.dismiss.mockClear();
-  Alert.alert.mockClear();
-
-  verifyEmail('notanamail');
-
-  expect(Keyboard.dismiss).toHaveBeenCalled();
-  expect(Alert.alert).toHaveBeenCalled();
-});
-
 it('sends a reminder then shows an alert', () => {
   Alert.alert = jest.fn();
   const mockDispatch = jest.fn(v => v);
 
   const { onPress } = mergeProps(
     { email: 'test@email.com' },
-    { dispatch: mockDispatch },
+    { dispatch: mockDispatch }
   );
 
   return onPress().then(() => {
