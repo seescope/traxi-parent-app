1aeaad5bef6978774af7f4b1ce26a51f3cfb8979
Kane Rogers <kane.m.rogers@gmail.com>
2017-09-05 12:23:01 +1000

    Fetch actions directly from Firebase.
    Who wants to lug around a giant JSON file of parent actions? Now we can
    fetch 'em straight from Firebase. Straight from _the future_.
    
    Or more accurately, the past.

--- App/Middleware/ReplayActions.js
+++ App/Middleware/ReplayActions.js
@@ -1,5 +1,7 @@
 // @flow
 import moment from 'moment';
+import _ from 'lodash';
+import { database } from 'firebase';
 import { Actions } from 'react-native-router-flux';
 
 type Action = {
@@ -13,14 +15,15 @@ type StoredAction = {
 };
 
 type FirebaseActions = {
-  [string]: ?{
-    [string]: StoredAction
-  }
+  [string]: StoredAction
 };
 
 type Dispatch = (action: Action) => any;
 
-const SCREEN_CHANGE = /REACT_NATIVE_ROUTER_FLUX_(REPLACE|PUSH|RESET)/;
+const PUSH = 'REACT_NATIVE_ROUTER_FLUX_PUSH';
+const RESET = 'REACT_NATIVE_ROUTER_FLUX_RESET';
+const REPLACE = 'REACT_NATIVE_ROUTER_FLUX_REPLACE';
+const BACK = 'REACT_NATIVE_ROUTER_FLUX_BACK_ACTION';
 
 const getSetupState = (actions: StoredAction[]): Action => {
   const userLoggedIn = actions.filter(a => a.action.match(/USER_LOGGED_IN/))[0];
@@ -36,8 +39,10 @@ const getSetupState = (actions: StoredAction[]): Action => {
 };
 
 const handleAction = (action: Action, dispatch: Dispatch) => {
-  if (action.type.match(SCREEN_CHANGE)) Actions[action.key]();
-  if (action.type === 'REACT_NATIVE_ROUTER_FLUX_BACK_ACTION') Actions.pop();
+  if (action.type === PUSH) Actions[action.key]();
+  if (action.type === RESET) Actions[action.key]({ type: 'reset' });
+  if (action.type === REPLACE) Actions[action.key]({ type: 'replace' });
+  if (action.type === BACK) Actions.pop();
   else dispatch(action);
 };
 
@@ -50,20 +55,39 @@ const dispatchActions = (actions: StoredAction[], dispatch: Dispatch) => {
 
   handleAction(parsedAction, dispatch);
 
-  if (actions.length < 2) return;
+  if (actions.length < 2) {
+    console.warn('Finished replaying actions');
+    return;
+  }
 
   const startTime = moment(timestamp);
   const nextTime = moment(actions[1].timestamp);
   const delta = nextTime.diff(startTime);
 
-  setTimeout(() => dispatchActions(actions.slice(1), dispatch), delta);
+  // If the next action is longer than a minute away, let's just say it's 5 seconds.
+  const timeout = delta > 60000 ? 5000 : delta;
+  if (delta > 60000) console.log('Long pause', delta / 1000, 'seconds');
+  console.log('Playing next action in', timeout / 1000, 'seconds');
+
+  setTimeout(() => dispatchActions(actions.slice(1), dispatch), timeout);
 };
 
-export default (uuid: string, actions: FirebaseActions, dispatch: Dispatch) => {
-  const actionsForUUID = actions[uuid];
-  if (!actionsForUUID) return;
+const fetchActions = (UUID: string): ?FirebaseActions =>
+  database().ref(`parentAppActions/${UUID}`).once('value').then(s => s.val());
 
-  const actionsArray = Object.keys(actionsForUUID).map(k => actionsForUUID[k]);
+export default async (uuid: string, dispatch: Dispatch) => {
+  console.warn('Replaying actions for', uuid);
+  const actionsForUUID = await fetchActions(uuid);
+  if (!actionsForUUID) {
+    console.error('No actions found!');
+    return;
+  }
+
+  const actionsArray = _.sortBy(
+    Object.keys(actionsForUUID).map(k => actionsForUUID[k]),
+    'timestamp'
+  );
+  console.log(`Replaying ${actionsArray.length} actions for ${uuid}`);
 
   const setupStateAction = getSetupState(actionsArray);
   dispatch(setupStateAction);
